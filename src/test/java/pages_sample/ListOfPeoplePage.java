4714c3b686f7df739034d6c58d2d5ba6d7b76fd1
gizemozdicle <gizemozdicle@gmail.com>
2022-08-11 15:58:29 +0300

    adadad

--- src/test/java/pages_sample/ListOfPeoplePage.java
+++ src/test/java/pages_sample/ListOfPeoplePage.java
@@ -3,6 +3,7 @@ package pages_sample;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.ui.Select;
 
 import static org.junit.Assert.*;
 
